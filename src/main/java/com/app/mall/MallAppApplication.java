e86527721a9c35ac81ae06664e8fe7c855797217
psyuhen <psyuhen@163.com>
2018-07-19 20:31:40 +0800

    初始化工程

---
+++ src/main/java/com/app/mall/MallAppApplication.java
@@ -0,0 +1,12 @@
+package com.app.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAppApplication.class, args);
+    }
+}
